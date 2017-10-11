9bd686d4029122d5f9405dab377a1d4627cf8881
Joshua Melville <joshua.melville@northwestern.edu>
2017-10-11 11:47:44 +0100

    improvements and fixing blog page

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 
-<footer class="col-md-12 text-center">
-	<div class="container"><?php posts_nav_link(); ?></div>
+<footer class="text-center">
+	<div><?php posts_nav_link(); ?></div>
 </footer>
 <?php wp_footer(); ?>
--- index.php
+++ index.php
@@ -3,19 +3,34 @@
 <?php include('nav.php'); ?>
 	<div class="main-panel">
 		 <!-- Start the Loop. -->
+		 <!-- <article>
+			 <div class="article-content text-center">
+				 <h1 class="front-page-header"><?php get_the_title(); ?></h1>
+			 </div>
+		 </article> -->
 	<?php if ( is_user_logged_in() ) { ?>
 	 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-		 <article>
-			 <aside class="article-date pull-left" >
-				 <div class="post-date post-author"><em style="background-image:url('<?php echo get_wp_user_avatar_src(); ?>');"></em><?php the_author(); ?></div>
-				 <div class="post-date"><em><?php the_time('jS') ?></em> <?php the_time('M, Y') ?></div>
-				 <div class="post-date post-comments"><a href="<?php the_permalink(); ?>"><em><?php echo get_comments_number(); ?></em></a> Comments</div>
+		 <article class="clearfix">
+			 <aside class="article-meta" >
+				 <div class="post-date post-author">
+					 <em style="background-image:url('<?php echo get_wp_user_avatar_src(); ?>');"></em>
+					 <?php the_author(); ?>
+				 </div>
+				 <div class="post-date">
+					 <em><?php the_time('jS') ?></em>
+					 <?php the_time('M, Y') ?>
+				 </div>
+				 <div class="post-date post-comments">
+					 <a href="<?php the_permalink(); ?>">
+						 <em><?php echo get_comments_number(); ?></em>
+					 </a> Comments
+				 </div>
 			 </aside>
-			 <div class="article-content pull-right">
+			 <div class="article-content">
 				 <h1 class="article-header"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
 				 <small><?php the_tags(); ?></small>
+				 <div class="entry"><?php the_content(); ?></div>
 			 </div>
-			 <div class="entry"><?php the_content(); ?></div>
 		 </article>
 	 <?php endwhile; ?>
 	 <?php else: ?>
